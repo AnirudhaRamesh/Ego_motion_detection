1190c1d90a0d92f1d576c71b211b40b004ed107a
AnirudhaRamesh <anirudha.ramesh@students.iiit.ac.in>
2019-10-09 22:19:59 +0530

    Code structure cleaned, redundant files removed

--- Matlab_code/PointEstimate.m
+++
@@ -1,5 +0,0 @@
-function WorldPoint = PointEstimate(invK, point, Height)
-    point = [point 1];
-    dirVector = invK * point';
-    WorldPoint = (- Height * dirVector / ([0,-1,0] * dirVector));
-end
